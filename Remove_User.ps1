41fb22a224c98146ea23278ec431d76b8b96ca75
SkullsuiT <nft.older@gmail.com>
2023-01-05 13:54:27 +0100

    Create Remove_User.ps1

---
+++ Remove_User.ps1
@@ -0,0 +1,31 @@
+# GUI permettant d'utiliser RoboCopy avec PowerShell
+# JFDS_DSIGE-DRU_2023 
+
+# Définir la "Microsoft .NET Core class"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+# Récupération du dossier utilisateur
+    $RootUserACL = "C:\Users\"
+        $UserACL = New-Object System.Windows.Forms.FolderBrowserDialog
+        $UserACL.Description = "Choisissez l'utilisateur"
+        $UserACL.ShowNewFolderButton = $true
+        $UserACL.SelectedPath = $RootUserACL
+        
+        $User = $UserACL.SelectedPath
+
+# Récupération des ACL du dossier sélectionné
+    $acl = Get-Acl $User
+
+# Préparer l'élévation de droit
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrateur","FullControl","Allow")
+
+    $acl.SetAccessRule($AccessRule)
+
+# Appliquer les droits au dossier
+    $acl | Set-Acl $User
+
+Pause
+
+# Suppression de ce dossier
+Get-ChildItem -Path $User -Recurse | Remove-Item -Force
